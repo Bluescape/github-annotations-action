408fd28679e8980cc051ef83823ed71274925f0b
Fiona <fiona.hu@bluescape.com>
2021-03-03 16:39:44 -0500

    Modified to allow for modular hooks

--- src/index.js
+++ src/index.js
@@ -2,42 +2,38 @@ const core = require('@actions/core')
 
 const main = async () => {
   const resultsPath = core.getInput('resultPath')
-  const results = require(resultsPath).results[0].suites
+  const codeceptOutput = core.getInput('codeceptInput') || undefined
+  let results
+  if (codeceptOutput) {
+    results = handleCodeceptOutput(resultsPath)
+  }
   results.forEach((suite) => {
-    const failedTests = []
-    const failedHooks = []
-
-    suite.beforeHooks.forEach((hook) => {
-      if (hook.fail) {
-        failedHooks.push(`${hook.title}\n${hook.err.message}\n`)
-      }
-    })
-    suite.tests.forEach((test) => {
-      if (test.fail) {
-        failedTests.push(`${test.title}\n${test.err.message}\n`)
-      }
-    })
-    suite.afterHooks.forEach((hook) => {
-      if (hook.fail) {
-        failedHooks.push(`${hook.title}\n${hook.err.message}\n`)
-      }
-    })
-    if (failedTests.length > 0) {
-      core.setFailed(
-        `${suite.title} - ${
-          failedTests.length
-        } test(s) failed: \n${failedTests.join('\n')}`
-      )
+    if (suite.failedTests.length > 0) {
+      core.setFailed(`${suite.title} - ${suite.failedTests.length} test(s) failed: \n${suite.failedTests.join('\n')}`)
     }
-
-    if (failedHooks.length > 0) {
-      core.setFailed(
-        `${suite.title} - ${
-          failedHooks.length
-        } hook(s) failed: \n${failedHooks.join('\n')}`
-      )
+    if (suite.failedHooks.length > 0) {
+      core.setFailed(`${suite.title} - ${suite.failedHooks.length} hook(s) failed: \n${suite.failedHooks.join('\n')}`)
     }
   })
 }
 
 main().catch((err) => core.setFailed(err.message))
+
+function handleCodeceptOutput (reportPath) {
+  const results = require(reportPath).results[0].suites
+  const output = []
+  results.forEach((suite) => {
+    const suiteOutput = { title: suite.title, failedTests: [], failedHooks: [] }
+    suite.beforeHooks.forEach((hook) => {
+      if (hook.fail) suiteOutput.failedHooks.push(`${hook.title}\n${hook.err.message}\n`)
+    })
+    suite.afterHooks.forEach((hook) => {
+      if (hook.fail) suiteOutput.failedHooks.push(`${hook.title}\n${hook.err.message}\n`)
+    })
+    suite.tests.forEach((test) => {
+      if (test.fail) suiteOutput.failedTests.push(`${test.title}\n${test.err.message}\n`)
+    })
+    output.append(suiteOutput)
+  })
+  return output
+}
